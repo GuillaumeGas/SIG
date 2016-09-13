f10021c7a791f724d2204a65837cc82a60012be2
GuillaumeGas <guillaume@gas28.net>
2016-09-13 12:03:34 +0200

    Ajout premier algo enveloppe convexe

---
+++ algogeo/src/algo/EnvConvexe.java
@@ -0,0 +1,68 @@
+package algo;
+
+import base.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class EnvConvexe {
+    public ArrayList<Point> listPoints;
+
+    public EnvConvexe(ArrayList<Point> points) {
+	this.listPoints = points;
+    }
+
+    public ArrayList<Point> getConvexe() {
+	ArrayList<Point> list = new ArrayList<> ();
+	//On va chercher le point de départ, le plus à gauche pour etre sur qu'il fasse partie de l'enveloppe
+	//On en profite pour générer une copie de la liste de points
+	Point base = this.listPoints.get(0);
+	int base_index = 0;
+	list.add(new Point(base.x, base.y));
+	for (int i = 1; i < this.listPoints.size(); i++) {
+	    Point p = this.listPoints.get(i);
+	    if (p.y < base.y || (p.y == base.y && p.x < base.x)) {
+		base = p;
+		base_index = i;
+	    }
+	    list.add(new Point(p.x, p.y));
+	}
+
+	Collections.sort(list);
+
+	ArrayList<Point> res = new ArrayList<> ();
+	res.add(base);
+	int list_size = list.size();
+	Point last_above = null;
+	Point current_point = null;
+	for (int i = 1; i < list_size; i++) {
+	    Point last_point = res.get(res.size()-1);
+	    current_point = this.listPoints.get(i);
+	    if (current_point.x > last_point.x || res.size() <= 2) {
+		if (last_above != null) {
+		    float a1 = this.calculateArea(base, last_above, last_point);
+		    float a2 = this.calculateArea(base, last_point, current_point);
+		    if (a2 < a1) {
+			System.out.println("We remove (" + last_point.x + ", " + last_point.y + ")");
+			res.remove(res.size()-1);
+			last_point = null;
+		    }
+		    last_above = null;
+		}
+		res.add(current_point);
+	    } else {
+		if (last_above == null)
+		    last_above = last_point;
+		res.add(current_point);
+	    }
+	}
+	return res;
+    }
+
+    private float calculateArea(Point p, Point p1, Point p2) {
+	float[] vec1 = new float[2];
+	float[] vec2 = new float[2];
+	vec1[0] = p1.x - p.x; vec1[1] = p1.y - p.y;
+	vec2[0] = p2.x - p.x; vec2[1] = p2.y - p.y;
+	return (vec1[0] * vec2[1]) - (vec2[0] * vec1[1]);
+    }
+}
